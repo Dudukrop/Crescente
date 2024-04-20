8c28fd525bd2b08bfbcf13cc41607ae92b2316db
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-20 14:53:18 -0300

    first commit

---
+++ Exercicio - Crescente/Entities/Crescente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Crescente.Entities
+{
+    class Crescente
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+
+        public Crescente(int n1, int n2)
+        {
+            N1 = n1;
+            N2 = n2;
+        }
+
+        public string Decrescente(int n1, int n2)
+        {
+            if (n1 > n2)
+                return "Decrescente";
+
+            return "Crescente";
+        }
+
+        public override string ToString()
+        {
+            return Decrescente(N1, N2);
+        }
+    }
+}
---
+++ Exercicio - Crescente/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exercicio___Crescente.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Crescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois numeros:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            while (n1 != n2)
+            {
+                Crescente crescente = new Crescente(n1, n2);
+                Console.WriteLine(crescente);
+
+                Console.WriteLine("Digite outros dois numeros:");
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
